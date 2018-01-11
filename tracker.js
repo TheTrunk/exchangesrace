512f2f72d0cb065c899b5ae2469d2d474029b2c6
TheTrunk <TheTrunk@users.noreply.github.com>
2018-01-11 19:26:04 +0100

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -1,53 +1,53 @@
-var BtczBalance1 = 0;
-var BtczBalance2 = 0;
-var BtczValue = 0;
-var startDate = new Date("2018-01-03T19:00:00Z");
+var HushBalance1 = 0;
+var HushBalance2 = 0;
+var HushValue = 0;
+var startDate = new Date("2018-01-11T19:00:00Z");
 
 function updatePage(){
     var currentDate = new Date().getTime();
     var hours = Number(Math.round((currentDate-startDate)/3600000));
-    var totalDonations = Number(Math.round(BtczBalance1+BtczBalance2));
+    var totalDonations = Number(Math.round(HushBalance1+HushBalance2));
     var hourlyAverage = Number(Math.round(totalDonations/hours));
-    var BtczBalance1_toBtc = (BtczBalance1*BtczValue);
-    var BtczBalance2_toBtc = (BtczBalance2*BtczValue);
-    document.getElementById('statsTxt').innerHTML = "<b>" + totalDonations + " BTCZ</b> raised in <b>" + hours + " hours</b>, average: <b>" + hourlyAverage + " BTCZ/h</b>";
-    document.getElementById('btczBalance1').innerHTML = BtczBalance1.toFixed(0) + " BTCZ";
-    document.getElementById('btczBalance2').innerHTML = BtczBalance2.toFixed(0) + " BTCZ";
-    document.getElementById('btczBalance1_toBtc').innerHTML = "<b>" + BtczBalance1_toBtc.toFixed(8) + " BTC</b>";
-    document.getElementById('btczBalance2_toBtc').innerHTML = "<b>" + BtczBalance2_toBtc.toFixed(8) + " BTC</b>";
-    document.getElementById('progressBar1').style.width = (BtczBalance1_toBtc/0.025).toFixed(2) + "%";
-    document.getElementById('progressPercent1').innerHTML = (BtczBalance1_toBtc/0.025).toFixed(0) + "%";
-    document.getElementById('progressBar2').style.width = (BtczBalance2_toBtc/0.025).toFixed(2) + "%";
-    document.getElementById('progressPercent2').innerHTML = (BtczBalance2_toBtc/0.025).toFixed(0) + "%";
+    var HushBalance1_toBtc = (HushBalance1*HushValue);
+    var HushBalance2_toBtc = (HushBalance2*HushValue);
+    document.getElementById('statsTxt').innerHTML = "<b>" + totalDonations + " HUSH</b> raised in <b>" + hours + " hours</b>, average: <b>" + hourlyAverage + " HUSH/h</b>";
+    document.getElementById('hushBalance1').innerHTML = HushBalance1.toFixed(0) + " HUSH";
+    document.getElementById('hushBalance2').innerHTML = HushBalance2.toFixed(0) + " HUSH";
+    document.getElementById('hushBalance1_toBtc').innerHTML = "<b>" + HushBalance1_toBtc.toFixed(8) + " BTC</b>";
+    document.getElementById('hushBalance2_toBtc').innerHTML = "<b>" + HushBalance2_toBtc.toFixed(8) + " BTC</b>";
+    document.getElementById('progressBar1').style.width = (HushBalance1_toBtc/0.025).toFixed(2) + "%";
+    document.getElementById('progressPercent1').innerHTML = (HushBalance1_toBtc/0.025).toFixed(0) + "%";
+    document.getElementById('progressBar2').style.width = (HushBalance2_toBtc/0.025).toFixed(2) + "%";
+    document.getElementById('progressPercent2').innerHTML = (HushBalance2_toBtc/0.025).toFixed(0) + "%";
 }
 
 function getBalances(){
     var xhr1 = new XMLHttpRequest();
-    xhr1.open("GET", "https://bitcoinz.ph/api/addr/t1L2h7WyVsxEvbw1acefwXWBQWYReQnN7MF/balance", true);
+    xhr1.open("GET", "https://explorer.myhush.org/api/addr/t1h6kmaQwcuyejDLazT3TNZfV8EEtCzHRhc/balance", true);
     xhr1.onload = function(){
         if(this.status == 200){
             var result = xhr1.responseText;
-            BtczBalance1 = Number(result/100000000);
+            HushBalance1 = Number(result/100000000);
             updatePage();
         }
     }
     xhr1.send();
     var xhr2 = new XMLHttpRequest();
-    xhr2.open("GET", "https://bitcoinz.ph/api/addr/t1QPbt39fb4pzWXkNSM3qo6dWiL4ii42s48/balance", true);
+    xhr2.open("GET", "https://explorer.myhush.org/api/addr/t1h6kmaQwcuyejDLazT3TNZfV8EEtCzHRhc/balance", true);
     xhr2.onload = function(){
         if(this.status == 200){
             var result = xhr2.responseText;
-            BtczBalance2 = Number(result/100000000);
+            HushBalance2 = Number(result/100000000);
             updatePage();
         }
     }
     xhr2.send();
     var xhr3 = new XMLHttpRequest();
-    xhr3.open("GET", "https://api.coinmarketcap.com/v1/ticker/bitcoinz/", true);
+    xhr3.open("GET", "https://api.coinmarketcap.com/v1/ticker/hush/", true);
     xhr3.onload = function(){
         if(this.status == 200){
             var result = JSON.parse(xhr3.responseText);
-            BtczValue = Number(result[0].price_btc);
+            HushValue = Number(result[0].price_btc);
             updatePage();
         }
     }
